c4d2a9c6bd64f961548ff1a54efb42b1cf72694f
jini4847-ship-it <jini4847@gmail.com>
2026-06-25 10:28:04 +0900

    Create etf.py

---
+++ etf.py
@@ -0,0 +1,12 @@
+import yfinance as yf
+
+def get_etf(ticker):
+    hist = yf.Ticker(ticker).history(period="2d")
+
+    today = hist["Close"].iloc[-1]
+    prev = hist["Close"].iloc[-2]
+
+    change = today - prev
+    pct = (change / prev) * 100
+
+    return today, change, pct
