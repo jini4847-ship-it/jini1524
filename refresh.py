a6e25ce91698736313ff4d9e14e9720c397cf07f
jini4847-ship-it <jini4847@gmail.com>
2026-06-26 23:22:33 +0900

    refresh.py 업데이트

--- refresh.py
+++ refresh.py
@@ -5,9 +5,10 @@ def get_access_token():
     url = "https://kauth.kakao.com/oauth/token"
 
     data = {
-        "grant_type": "refresh_token",
-        "client_id": REST_API_KEY,
-        "refresh_token": REFRESH_TOKEN
+    "grant_type": "refresh_token",
+    "client_id": REST_API_KEY,
+    "client_secret": CLIENT_SECRET,
+    "refresh_token": REFRESH_TOKEN
     }
 
     response = requests.post(url, data=data)
